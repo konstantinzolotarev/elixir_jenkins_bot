95250ccb8868e085fa299f36381244ebc4e0d20a
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2016-08-29 17:22:49 +0300

    fix configs

--- config/config.exs
+++ config/config.exs
@@ -8,7 +8,7 @@ use Mix.Config
 # if you want to provide default values for your application for
 # 3rd-party users, it should be done in your "mix.exs" file.
 
-config :nadia
+config :nadia,
   token: "260849538:AAGGd7vn5bbtumdL_h6CFlucKZ4nAnIV-wQ"
 # You can configure for your application as:
 #
